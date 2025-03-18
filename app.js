1611b57415aa2fe86ab352b9d5d87f5646f232c8
markglasman <glasman.mark@gmail.com>
2025-03-18 12:25:53 -0400

    more server troubleshooting

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const path = require('path')
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html')
+// })
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 app.listen(port, () => {
