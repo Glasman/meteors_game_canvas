8cf3ada8b328729146b72f8dfddac845e5effab6
markglasman <glasman.mark@gmail.com>
2025-03-25 12:05:16 -0400

    express testing

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 //   res.sendFile(__dirname + '/index.html')
 // })
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
