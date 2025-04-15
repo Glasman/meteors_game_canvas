20eb40755da9d13ba465f8c869d52c0502b43145
markglasman <glasman.mark@gmail.com>
2025-04-15 13:18:21 -0400

    projectiles deleted from backend when fired in any direction

--- server.js
+++ server.js
@@ -89,12 +89,18 @@ setInterval(() => {
     backEndProjectiles[id].x += backEndProjectiles[id].velocity.x;
     backEndProjectiles[id].y += backEndProjectiles[id].velocity.y;
 
-    const PROJECTILE_RADIUS = 5
-    if (backEndProjectiles[id].x - PROJECTILE_RADIUS >= 
-      backEndPlayers[backEndProjectiles[id].playerId]?.canvas?.width) {
-        delete backEndProjectiles[id]
+    const PROJECTILE_RADIUS = 5;
+    if (
+      backEndProjectiles[id].x - PROJECTILE_RADIUS >=
+        backEndPlayers[backEndProjectiles[id].playerId]?.canvas?.width ||
+      backEndProjectiles[id].x + PROJECTILE_RADIUS <= 0 ||
+      backEndProjectiles[id].y - PROJECTILE_RADIUS >=
+        backEndPlayers[backEndProjectiles[id].playerId]?.canvas?.height ||
+      backEndProjectiles[id].y + PROJECTILE_RADIUS <= 0
+    ) {
+      delete backEndProjectiles[id];
     }
-    console.log(backEndProjectiles)
+    console.log(backEndProjectiles);
   }
 
   io.emit("updatePlayers", backEndPlayers);
